ceaf765164b8b8fa68815d28255aae9e3fb1ab22
jeroenknol <jeroen-knol@live.nl>
2022-06-22 14:50:35 +0200

    basic styling setup

---
+++ components/todos.tsx
@@ -0,0 +1,7 @@
+export const Todos = () => {
+  return (
+    <main className='w-96 h-[768px] bg-slate-800 rounded-lg p-4'>
+      Todo list
+    </main>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
+import { Todos } from '../components/todos';
 
 const Home: NextPage = () => {
-  return <div className='text-red-500'>test</div>;
+  return (
+    <div className='h-[100vh] bg-slate-900 flex justify-center items-center'>
+      <Todos />
+    </div>
+  );
 };
 
 export default Home;
